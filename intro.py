d0e6c2e0925540ac40e741f533a05a0362c34917
root <gauravkumarsl.a00@gmail.com>
2025-04-05 14:00:33 +0000

    third

--- intro.py
+++ intro.py
@@ -5,35 +5,22 @@ import os
 
 if not os.environ.get("GROQ_API_KEY"):
   os.environ["GROQ_API_KEY"] = "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"
-
+import langchain
 from langchain.chat_models import init_chat_model
 from langchain_core.prompts import ChatPromptTemplate
 
-example_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("human", "{input}"),
-        ("ai", "{output}"),
-    ]
-)
+prompt_template = ChatPromptTemplate([
+    ("system", "You are a helpful assistant"),
+    ("placeholder", "{msgs}") # <-- This is the changed part
+])
 
-# Example conversation
-examples = [
-    {"input": "What's 2 + 2?", "output": "The answer is 4."},
-    {"input": "Who discovered gravity?", "output": "Isaac Newton discovered gravity."},
-]
 
 # Initialize the model
 model = init_chat_model("llama3-8b-8192", model_provider="groq")
 
-# Format examples into the template
-formatted_examples = [example_prompt.format(**ex) for ex in examples]
-
-# New question to ask
-new_input = {"input": "Who is the founder of Microsoft?"}
-
 # Create a chain and invoke
-chain = example_prompt | model
-response = chain.invoke(new_input)
+chain = prompt_template | model
+response = chain.invoke("hello")
 
 # Print the response
 print(response.content)
