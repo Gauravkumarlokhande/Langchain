0f6b2994cf6686bd92d7a2e4334d75c9059f1ffd
root <gauravkumarsl.a00@gmail.com>
2025-04-05 15:12:02 +0000

    new

--- intro.py
+++ intro.py
@@ -1,27 +1,31 @@
 
-
-import getpass
 import os
-
-if not os.environ.get("GROQ_API_KEY"):
-  os.environ["GROQ_API_KEY"] = "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"
-import langchain
 from langchain.chat_models import init_chat_model
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+
+
+
 
 prompt_template = ChatPromptTemplate([
     ("system", "You are a helpful assistant"),
-    ("placeholder", "{msgs}") # <-- This is the changed part
+    ("placeholder", "{msgs}") 
 ])
 
 
+load_dotenv()
+
+groq_api_key = os.getenv('GROQ_API_KEY')
+
 # Initialize the model
-model = init_chat_model("llama3-8b-8192", model_provider="groq")
+model = ChatGroq(temperature=0, groq_api_key=groq_api_key, model_name="llama3-8b-8192")
 
 # Create a chain and invoke
 chain = prompt_template | model
-response = chain.invoke("hello")
+input_data = {"msgs": "Your message or conversation history here"}
 
-# Print the response
+# Invoke the chain with the input data
+response = chain.invoke({"msgs": [HumanMessage(content="hi!")]})
 print(response.content)
-# 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
